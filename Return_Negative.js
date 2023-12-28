9052cfbc8f97b7878120715dce89a64d43b21ddf
sh-sl <vshedu@gmail.com>
2023-12-28 14:59:36 +0100

    Return Negative

---
+++ Return_Negative.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// SOLUTION
+
+function makeNegative(num) {
+  return num < 0 ? num : num * -1;
+}
